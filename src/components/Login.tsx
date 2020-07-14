fd3cae99bd5a4f5e67f9db022fe6fa1ae79fd06c
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-14 17:00:51 +0200

    basic template for login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,21 @@
 import React, { FunctionComponent } from 'react';
+import { Paper, Typography, Divider } from '@material-ui/core';
 
 const Login: FunctionComponent = () => {
-	return <div>Hello!</div>;
+	return (
+		<div className="login-container">
+			<div className="login-container__img"></div>
+			<Paper className="login-container__box">
+				<Typography
+					variant="h2"
+					className="heading-2 login-container__login_heading"
+				>
+					Zaloguj się
+				</Typography>
+				<Divider />
+			</Paper>
+		</div>
+	);
 };
 
 export default Login;
