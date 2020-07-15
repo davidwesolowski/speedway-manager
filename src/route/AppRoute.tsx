606a86e6e7449cf34a93bf9ebf9795ff80279c25
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-16 00:38:05 +0200

    changed files js to tsx

---
+++ src/route/AppRoute.tsx
@@ -0,0 +1,21 @@
+import React, { FunctionComponent } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import WelcomePage from '../components/WelcomePage';
+import Login from '../components/Login';
+
+const AppRoute: FunctionComponent = () => {
+	return (
+		<Router>
+			<Header />
+			<Switch>
+				<Route path="/" exact component={WelcomePage} />
+				<Route path="/login" component={Login} />
+			</Switch>
+			<Footer />
+		</Router>
+	);
+};
+
+export default AppRoute;
