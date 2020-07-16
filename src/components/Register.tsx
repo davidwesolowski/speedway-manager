4bb1c27a034669d00a1a5816600364178838d3b4
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-16 23:36:52 +0200

    Created basic template of Register component

---
+++ src/components/Register.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+const Register: FunctionComponent<RouteComponentProps> = props => {
+	return <div>Hello</div>;
+};
+
+export default Register;
