adfaa3b293b4abcfb20d729d521fd67b7172f18c
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-17 00:19:08 +0200

    Created centered box with heading and styles for these elements

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,8 +1,22 @@
 import React, { FunctionComponent } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Paper, Typography, Divider } from '@material-ui/core';
 
 const Register: FunctionComponent<RouteComponentProps> = props => {
-	return <div>Hello</div>;
+	return (
+		<div className="register-container">
+			<div className="register-container__img"></div>
+			<Paper className="register-container__box">
+				<Typography
+					variant="h2"
+					className="heading-1 register-container__heading"
+				>
+					Rejestracja
+				</Typography>
+				<Divider />
+			</Paper>
+		</div>
+	);
 };
 
 export default Register;
