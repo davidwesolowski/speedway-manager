17b2853dac6dc62cef57102646abbec72f93169b
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-20 16:34:39 +0200

    Changed header to AppBar component
    Added flexGrow 1 to nav elements and styles for toolbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,36 @@
 import React, { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar } from '@material-ui/core';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header: FunctionComponent = () => {
 	return (
-		<header className="header">
-			<div className="header__logo">LOGO</div>
-			<ul className="header__nav">
-				<li className="header__item">
-					<Link to="/" className="header__link">
-						Start
-					</Link>
-				</li>
-				<li className="header__item">
-					<Link to="/" className="header__link">
-						Ranking Ekstraligi
-					</Link>
-				</li>
-				<li className="header__item">
-					<Link to="/" className="header__link">
-						Wyniki meczów
-					</Link>
-				</li>
-			</ul>
-			<Link to="/login" className="header__link header__login">
-				Zaloguj
-			</Link>
-		</header>
+		<AppBar position="sticky" className="header">
+			<Toolbar className="header__toolbar">
+				<div className="header__logo">LOGO</div>
+				<ul className="header__nav">
+					<li className="header__item">
+						<Link to="/" className="header__link">
+							Start
+						</Link>
+					</li>
+					<li className="header__item">
+						<Link to="/" className="header__link">
+							Ranking Ekstraligi
+						</Link>
+					</li>
+					<li className="header__item">
+						<Link to="/" className="header__link">
+							Wyniki meczów
+						</Link>
+					</li>
+				</ul>
+				<Link to="/login" className="header__link header__login">
+					<div>Zaloguj</div>
+					<FaUserCircle />
+				</Link>
+			</Toolbar>
+		</AppBar>
 	);
 };
 
