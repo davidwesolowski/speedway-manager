0139c2599c80eb597ba2536c641e0052a196af7e
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-24 17:26:55 +0200

    Added interface of state and default state

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent} from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import {
 	Paper,
 	Typography,
@@ -7,74 +7,115 @@ import {
 	IconButton,
 	Button
 } from '@material-ui/core';
-import {Link} from 'react-router-dom';
-import {TiPen, TiTimes} from 'react-icons/ti';
+import { Link } from 'react-router-dom';
+import { TiPen, TiTimes } from 'react-icons/ti';
+
+interface IState {
+	username: string;
+	name: string;
+	club: string;
+	points: number;
+	position: number;
+}
 
 const Account: FunctionComponent = () => {
-    
-    const AccountData = {
-        Username: "Nazwa użytkownika",
-        Name: "Imię",
-        Club: "Klub",
-        Points: 45,
-        Position: 1,
-    };
+	const accountDefaultData = {
+		username: '',
+		name: '',
+		club: '',
+		points: 0,
+		position: -1
+	};
+	const [accountData, setAccountData] = useState<IState>(accountDefaultData);
 
-    return(
-        <div className="account-info">
-            <div className="account-info__background"></div>
-            <Paper className="account-info__box">
-                <Typography variant="h2" className="account-info__header">
-                    {AccountData.Username} ({AccountData.Name})
-                </Typography>
-                <Divider />
-                <div className="account-info__avatar-part">
-                    <div className="account-info__avatar-img-box">
-                        <img src="/img/kenny.jpg" className="account-info__avatar-img"></img>
-                    </div>
-                    <Link to="/zmien-awatar" className="account-info__change-avatar">
-                        Zmień swój awatar
-                    </Link>
-                </div>
-                <div className="account-info__account-data-part">
-                    <div className="account-info__nickname-row">
-                        <div className="account-info__username"></div>
-                        <div className="account-info__change-nickname-part">
-                            <Link to="/edycja-konta" className="account-info__change-nickname"><TiPen></TiPen> Edytuj konto</Link>
-                            <br/>
-                            <br/>
-                            <Link to="/usuwanie-konta" className="account-info__delete-account"><TiTimes></TiTimes> Usuń konto</Link>
-                        </div>
-                    </div>
-                    <br/>
-                    <br/>
-                    <div>
-                        <Typography variant="h5"  className="account-info__stats">
-                             <strong>Nazwa klubu:</strong>  <Link to="/klub" className="account-info__club">{AccountData.Club}</Link>
-                        </Typography>
-                    </div>
-                    <br/>
-                    <div>
-                        <Typography variant="h5"  className="account-info__stats">
-                             <strong>Punkty:</strong> {AccountData.Points}
-                        </Typography>
-                    </div>
-                    <br/>
-                    <div>
-                        <Typography variant="h5"  className="account-info__stats">
-                             <strong>Pozycja w rankingu:</strong> {AccountData.Position}
-                        </Typography>
-                    </div>
-                    <br/>
-                    <div>
-                        <Typography variant="h5"  className="account-info__stats">
-                             <strong>Hasło:</strong> ***********   
-                        </Typography>
-                    </div>
-                </div>
-            </Paper>
-        </div>
-    );
+	return (
+		<div className="account-info">
+			<div className="account-info__background"></div>
+			<Paper className="account-info__box">
+				<Typography variant="h2" className="account-info__header">
+					{accountData.username} ({accountData.name})
+				</Typography>
+				<Divider />
+				<div className="account-info__avatar-part">
+					<div className="account-info__avatar-img-box">
+						<img
+							src="/img/kenny.jpg"
+							alt="user-avatar"
+							className="account-info__avatar-img"
+						></img>
+					</div>
+					<Link
+						to="/zmien-awatar"
+						className="account-info__change-avatar"
+					>
+						Zmień swój awatar
+					</Link>
+				</div>
+				<div className="account-info__account-data-part">
+					<div className="account-info__nickname-row">
+						<div className="account-info__username"></div>
+						<div className="account-info__change-nickname-part">
+							<Link
+								to="/edycja-konta"
+								className="account-info__change-nickname"
+							>
+								<TiPen></TiPen> Edytuj konto
+							</Link>
+							<br />
+							<br />
+							<Link
+								to="/usuwanie-konta"
+								className="account-info__delete-account"
+							>
+								<TiTimes></TiTimes> Usuń konto
+							</Link>
+						</div>
+					</div>
+					<br />
+					<br />
+					<div>
+						<Typography
+							variant="h5"
+							className="account-info__stats"
+						>
+							<strong>Nazwa klubu:</strong>{' '}
+							<Link to="/klub" className="account-info__club">
+								{accountData.club}
+							</Link>
+						</Typography>
+					</div>
+					<br />
+					<div>
+						<Typography
+							variant="h5"
+							className="account-info__stats"
+						>
+							<strong>Punkty:</strong> {accountData.points}
+						</Typography>
+					</div>
+					<br />
+					<div>
+						<Typography
+							variant="h5"
+							className="account-info__stats"
+						>
+							<strong>Pozycja w rankingu:</strong>{' '}
+							{accountData.position}
+						</Typography>
+					</div>
+					<br />
+					<div>
+						<Typography
+							variant="h5"
+							className="account-info__stats"
+						>
+							<strong>Hasło:</strong> ***********
+						</Typography>
+					</div>
+				</div>
+			</Paper>
+		</div>
+	);
 };
 
 //do linii 41: <Typography variant="h3"> {AccountData.Username} ({AccountData.Name}) </Typography>
