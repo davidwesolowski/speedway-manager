e5a86eca3e91f7d7b734bb028a2554202dae0059
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-24 18:12:47 +0200

    Added dialog, dialog title and styles for these

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { FunctionComponent, useState } from 'react';
 import {
 	Paper,
@@ -5,10 +7,13 @@ import {
 	Divider,
 	TextField,
 	IconButton,
-	Button
+	Button,
+	Dialog,
+	DialogTitle
 } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { TiPen, TiTimes } from 'react-icons/ti';
+import { FiX } from 'react-icons/fi';
 
 interface IState {
 	username: string;
@@ -27,88 +32,109 @@ const Account: FunctionComponent = () => {
 		position: -1
 	};
 	const [accountData, setAccountData] = useState<IState>(accountDefaultData);
+	const [dialogOpen, setDialogOpen] = useState<boolean>(true);
+
+	const handleOpen = () => setDialogOpen(true);
+	const handleClose = () => setDialogOpen(false);
 
 	return (
-		<div className="account-info">
-			<div className="account-info__background"></div>
-			<Paper className="account-info__box">
-				<Typography variant="h2" className="account-info__header">
-					{accountData.username} ({accountData.name})
-				</Typography>
-				<Divider />
-				<div className="account-info__avatar-part">
-					<div className="account-info__avatar-img-box">
-						<img
-							src="/img/kenny.jpg"
-							alt="user-avatar"
-							className="account-info__avatar-img"
-						></img>
+		<>
+			<div className="account-info">
+				<div className="account-info__background"></div>
+				<Paper className="account-info__box">
+					<Typography variant="h2" className="account-info__header">
+						{accountData.username} ({accountData.name})
+					</Typography>
+					<Divider />
+					<div className="account-info__avatar-part">
+						<div className="account-info__avatar-img-box">
+							<img
+								src="/img/kenny.jpg"
+								alt="user-avatar"
+								className="account-info__avatar-img"
+							></img>
+						</div>
+						<Link
+							to="/zmien-awatar"
+							className="account-info__change-avatar"
+						>
+							Zmień swój awatar
+						</Link>
 					</div>
-					<Link
-						to="/zmien-awatar"
-						className="account-info__change-avatar"
-					>
-						Zmień swój awatar
-					</Link>
-				</div>
-				<div className="account-info__account-data-part">
-					<div className="account-info__nickname-row">
-						<div className="account-info__username"></div>
-						<div className="account-info__change-nickname-part">
-							<div className="account-info__change-nickname">
-								<TiPen /> Edytuj konto
-							</div>
-							<br />
-							<br />
-							<div className="account-info__delete-account">
-								<TiTimes /> Usuń konto
+					<div className="account-info__account-data-part">
+						<div className="account-info__nickname-row">
+							<div className="account-info__username"></div>
+							<div className="account-info__change-nickname-part">
+								<div
+									className="account-info__change-nickname"
+									onClick={handleOpen}
+								>
+									<TiPen /> Edytuj konto
+								</div>
+								<br />
+								<br />
+								<div className="account-info__delete-account">
+									<TiTimes /> Usuń konto
+								</div>
 							</div>
 						</div>
+						<br />
+						<br />
+						<div>
+							<Typography
+								variant="h5"
+								className="account-info__stats"
+							>
+								<strong>Nazwa klubu:</strong>{' '}
+								<Link to="/klub" className="account-info__club">
+									{accountData.club}
+								</Link>
+							</Typography>
+						</div>
+						<br />
+						<div>
+							<Typography
+								variant="h5"
+								className="account-info__stats"
+							>
+								<strong>Punkty:</strong> {accountData.points}
+							</Typography>
+						</div>
+						<br />
+						<div>
+							<Typography
+								variant="h5"
+								className="account-info__stats"
+							>
+								<strong>Pozycja w rankingu:</strong>{' '}
+								{accountData.position}
+							</Typography>
+						</div>
+						<br />
+						<div>
+							<Typography
+								variant="h5"
+								className="account-info__stats"
+							>
+								<strong>Hasło:</strong> ***********
+							</Typography>
+						</div>
 					</div>
-					<br />
-					<br />
-					<div>
-						<Typography
-							variant="h5"
-							className="account-info__stats"
-						>
-							<strong>Nazwa klubu:</strong>{' '}
-							<Link to="/klub" className="account-info__club">
-								{accountData.club}
-							</Link>
+				</Paper>
+			</div>
+			<Dialog open={dialogOpen} onClose={handleClose} className="dialog">
+				<DialogTitle>
+					<div className="dialog__header">
+						<Typography variant="h4" className="dialog__title">
+							Edycja konta
 						</Typography>
+						<IconButton>
+							<FiX />
+						</IconButton>
 					</div>
-					<br />
-					<div>
-						<Typography
-							variant="h5"
-							className="account-info__stats"
-						>
-							<strong>Punkty:</strong> {accountData.points}
-						</Typography>
-					</div>
-					<br />
-					<div>
-						<Typography
-							variant="h5"
-							className="account-info__stats"
-						>
-							<strong>Pozycja w rankingu:</strong>{' '}
-							{accountData.position}
-						</Typography>
-					</div>
-					<br />
-					<div>
-						<Typography
-							variant="h5"
-							className="account-info__stats"
-						>
-							<strong>Hasło:</strong> ***********
-						</Typography>
-					</div>
-				</div>
-			</Paper>
-		</div>
+				</DialogTitle>
+			</Dialog>
+		</>
 	);
 };
 
