c8e385ba38f4d3eae74b9a56dabb6bcc4b67e494
Dominik Kubiaczyk <domino.98@o2.pl>
2020-07-26 12:18:01 +0200

    Added data types to adding and deleting riders

--- src/components/Riders.tsx
+++ src/components/Riders.tsx
@@ -1,4 +1,5 @@
-import React, {FunctionComponent} from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import {
 	Paper,
 	Typography,
@@ -7,6 +8,95 @@ import {
 	IconButton,
 	Button
 } from '@material-ui/core';
+import {FiPlus} from 'react-icons/fi';
+
+interface IRider{
+    firstName: string;
+    lastName: string;
+    nickname: string;
+    dateOfBirth: Date;
+    club: string;
+}
+
+interface IValidatedData{
+    firstName: {
+        message: string;
+        error: boolean;
+    };
+    lastName: {
+        message: string;
+        error: boolean;
+    };
+    nickname: {
+        message: string;
+        error: boolean;
+    };
+    dateOfBirth: {
+        message: string;
+        error: boolean;
+    };
+    club: {
+        message: string;
+        error: boolean;
+    };
+}
+
+const AddRider: FunctionComponent<RouteComponentProps> = ({
+    history: { push }
+}) => {
+    const defaultValidatedData = {
+        firstName: {
+            message: '',
+            error: false
+        },
+        lastName: {
+            message: '',
+            error: false
+        },
+        nickname: {
+            message: '',
+            error: false
+        },
+        dateOfBirth: {
+            message: '',
+            error: false
+        },
+        club: {
+            message: '',
+            error: false
+        }
+    };
+
+    const [riderData, setRiderData] = useState<IRider>({
+        firstName: '',
+        lastName: '',
+        nickname: '',
+        dateOfBirth: new Date(1900,1,1),
+        club: ''
+    });
+    const [validatedData, setValidatedData] = useState<IValidatedData>(
+        defaultValidatedData
+    );
+    const [addRiderSuccess, setAddRiderSuccess] = useState<boolean>(false);
+    const [addRiderError, setAddRiderError] = useState<boolean>(false);
+    const [showDialog, setShowDialog] = useState<boolean>(false);
+
+    const handleOnChange = (name: string) => (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        event.persist();
+        if (event.target) {
+            setRiderData((prevState: IRider) => ({
+                ...prevState,
+                [name]: event.target.value
+            }));
+        }
+    };
+
+    return(
+        <div></div>
+    );
+}
 
 const Riders: FunctionComponent = () => {
     
@@ -18,6 +108,9 @@ const Riders: FunctionComponent = () => {
                     Dodawanie zawodnika
                 </Typography>
                 <Divider />
+                <Typography variant="h3">
+                    <FiPlus className="riders__fiplus" />
+                </Typography>
             </Paper>
         </div>
     );
