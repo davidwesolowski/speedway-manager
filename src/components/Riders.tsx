fc491921d11a1a3da18e81fc96e0c93ab8e82bf8
Dominik Kubiaczyk <domino.98@o2.pl>
2020-07-26 13:52:15 +0200

    Added empty form for adding new riders

--- src/components/Riders.tsx
+++ src/components/Riders.tsx
@@ -6,10 +6,18 @@ import {
 	Divider,
 	TextField,
 	IconButton,
-	Button
+	Button,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	FormControl,
+	Grid,
+	InputAdornment
 } from '@material-ui/core';
-import {FiPlus} from 'react-icons/fi';
+import {FiPlus, FiX} from 'react-icons/fi';
 import axios from 'axios';
+import validateRiderData from '../validation/validateRiderData';
+import { ValidationErrorItem } from '@hapi/joi';
 
 interface IRider{
     firstName: string;
@@ -42,32 +50,30 @@ interface IValidatedData{
     };
 }
 
-const Riders: FunctionComponent<RouteComponentProps> = ({
-    history: { push }
-}) => {
-    const defaultValidatedData = {
-        firstName: {
-            message: '',
-            error: false
-        },
-        lastName: {
-            message: '',
-            error: false
-        },
-        nickname: {
-            message: '',
-            error: false
-        },
-        dateOfBirth: {
-            message: '',
-            error: false
-        },
-        club: {
-            message: '',
-            error: false
-        }
-    };
+const defaultValidatedData = {
+    firstName: {
+        message: '',
+        error: false
+    },
+    lastName: {
+        message: '',
+        error: false
+    },
+    nickname: {
+        message: '',
+        error: false
+    },
+    dateOfBirth: {
+        message: '',
+        error: false
+    },
+    club: {
+        message: '',
+        error: false
+    }
+};
 
+const Riders: FunctionComponent = () => {
     const [riderData, setRiderData] = useState<IRider>({
         firstName: '',
         lastName: '',
@@ -81,6 +87,12 @@ const Riders: FunctionComponent<RouteComponentProps> = ({
     const [addRiderSuccess, setAddRiderSuccess] = useState<boolean>(false);
     const [addRiderError, setAddRiderError] = useState<boolean>(false);
     const [showDialog, setShowDialog] = useState<boolean>(false);
+
+    const handleOpen = () => setShowDialog(true);
+    const handleClose = () => {
+        setValidatedData(defaultValidatedData);
+        setShowDialog(false);
+    };
 
     const handleOnChange = (name: string) => (
         event: React.ChangeEvent<HTMLInputElement>
@@ -112,20 +124,67 @@ const Riders: FunctionComponent<RouteComponentProps> = ({
         }
     };
 
+    const handleOnSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        const validationResponse = validateRiderData(riderData);
+        if (validationResponse.error) {
+            setValidatedData(() => defaultValidatedData);
+            validationResponse.error.details.forEach(
+                (errorItem: ValidationErrorItem): any => {
+                    setValidatedData((prevState: IValidatedData) => {
+                        return {
+                            ...prevState,
+                            [errorItem.path[0]]: {
+                                message: errorItem.message,
+                                error: true
+                            }
+                        };
+                    });
+                }
+            );
+        } else {
+            const {firstName, lastName, nickname, dateOfBirth, club} = riderData;
+            addRider({firstName, lastName, nickname, dateOfBirth, club});
+        }
+    };
+
     return(
-        <div className="riders">
-            <div className="riders__background"></div>
-            <Paper className="riders__box">
-                <Typography variant="h2" className="riders__header">
-                    Dodawanie zawodnika
-                </Typography>
-                <Divider />
-                <Typography variant="h3">
-                    <FiPlus className="riders__fiplus" />
-                </Typography>
-            </Paper>
-        </div>
-    )
+        <>
+            <div className="riders">
+                <div className="riders__background"></div>
+                <Paper className="riders__box">
+                    <Typography variant="h2" className="riders__header">
+                        Zawodnicy
+                    </Typography>
+                    <Divider />
+                    <IconButton className="riders__fiplus" onClick={handleOpen}>
+                        <FiPlus />
+                    </IconButton>
+                </Paper>
+            </div>
+            <Dialog open={showDialog} onClose={handleClose} className="dialog">
+                <DialogTitle>
+                    <div className="dialog__header">
+                        <Typography variant="h4" className="dialog__title">
+                            Dodawanie zawodnika
+                        </Typography>
+                        <IconButton onClick={handleClose}>
+                            <FiX />
+                        </IconButton>
+                    </div>
+                </DialogTitle>
+                <DialogContent dividers>
+                    <form className="dialog__form" onSubmit={handleOnSubmit}>
+                        <Grid container>
+                            <Grid item xs={7} className="dialog__form_fields">
+                                
+                            </Grid>
+                        </Grid>
+                    </form> 
+                </DialogContent>
+            </Dialog>
+        </>
+    );
 };
 
 export default Riders;
