0f1cb83fa6c1ebcf8b78dd317879a20d68664500
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-29 10:18:45 +0200

    Added css styles for notifications

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './scss/main.scss';
+import 'react-notifications-component/dist/theme.css';
+import 'animate.css/animate.compat.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
