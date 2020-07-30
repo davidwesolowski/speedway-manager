a3dfcdcfa2a569c65de081907ba1e27b7ac80bec
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-30 13:46:19 +0200

    Created basic Team component

---
+++ src/components/Team.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+const Team: FunctionComponent<RouteComponentProps> = () => {
+	return <div></div>;
+};
+
+export default Team;
