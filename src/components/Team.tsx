1353af7bf1c700d9677af0a5a51f92cdb45b522c
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-30 14:29:37 +0200

    Added background and title

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,8 +1,22 @@
 import React, { FunctionComponent } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Paper, Typography, Divider } from '@material-ui/core';
 
 const Team: FunctionComponent<RouteComponentProps> = () => {
-	return <div></div>;
+	return (
+		<div className="team-container">
+			<div className="team-container__img"></div>
+			<Paper className="team-container__box">
+				<Typography
+					variant="h2"
+					className="heading-1 team-container__heading"
+				>
+					Swórz swoją drużynę marzeń!
+				</Typography>
+				<Divider />
+			</Paper>
+		</div>
+	);
 };
 
 export default Team;
