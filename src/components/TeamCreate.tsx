e76ada11bdc0384e740e0c0ddd74286ce443376a
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-07-31 10:34:54 +0200

    Added form with text field for name and select filed for league

--- src/components/TeamCreate.tsx
+++ src/components/TeamCreate.tsx
@@ -1,7 +1,55 @@
 import React, { FunctionComponent } from 'react';
+import {
+	Typography,
+	FormControl,
+	Grid,
+	TextField,
+	Select,
+	MenuItem,
+	InputLabel
+} from '@material-ui/core';
 
 const TeamCreate: FunctionComponent = () => {
-	return <div>Stwórz drużynę</div>;
+	return (
+		<div className="team-create-contaier">
+			<Typography className="heading-3 team-create-container__heading">
+				Stwórz drużynę
+			</Typography>
+			<form
+				className="team-create-container__form"
+				encType="multipart/form-data"
+			>
+				<Grid container justify="center">
+					<Grid
+						item
+						xs={5}
+						className="team-create-container__form-fields"
+					>
+						<FormControl className="team-create-container__form-field">
+							<TextField label="Nazwa" required />
+						</FormControl>
+						<FormControl
+							required
+							className="team-create-container__form-field"
+						>
+							<InputLabel id="id-league">Liga</InputLabel>
+							<Select labelId="id-league">
+								<MenuItem className="team-create-container__form-menu">
+									PGE Ekstraliga
+								</MenuItem>
+								<MenuItem className="team-create-container__form-menu">
+									eWINNER 1. Liga
+								</MenuItem>
+								<MenuItem className="team-create-container__form-menu">
+									2. Liga żużlowa
+								</MenuItem>
+							</Select>
+						</FormControl>
+					</Grid>
+				</Grid>
+			</form>
+		</div>
+	);
 };
 
 export default TeamCreate;
