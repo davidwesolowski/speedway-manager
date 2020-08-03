3c1dd1d30c8340d5c8876d826ca72f9ef5da40e6
Dominik Kubiaczyk <domino.98@o2.pl>
2020-08-03 17:06:27 +0200

    Fixed problems with getting riders from database

--- src/components/RidersList.tsx
+++ src/components/RidersList.tsx
@@ -3,106 +3,11 @@ import FiX from 'react-icons/fi';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 
-/*const RidersList: FunctionComponent = () => {
-    
-    interface IRider{
-        imię: string,
-        nazwisko: string,
-        przydomek: string,
-        data_urodzenia: string
-    }
-
-    const [riders, setRiders] = useState<Array<IRider>>();
-
-    const getRiders = async () => {
-        try {
-            const cookies = new Cookies();
-            const access_token = cookies.get("access_token");
-            const options = {
-                headers: {
-                    Authorization: `Bearer ${access_token}`
-                }
-            };
-            const {
-                data
-            } = await axios.get(
-                'https://fantasy-league-eti.herokuapp.com/riders',
-                options
-            );
-            setRiders([]);
-            data.map((rider) => {
-                setRiders(
-                        riders.concat({
-                        imię: rider.first_name,
-                        nazwisko: rider.last_name,
-                        przydomek: rider.nickname,
-                        data_urodzenia: rider.date_of_birth
-                    })
-                );
-            });
-
-            //setAddRiderError(false);
-            //setAddRiderSuccess(true);
-        } catch (e) {
-            //setAddRiderSuccess(false);
-            //setAddRiderError(true);
-            console.log(e.response);
-            throw new Error('Error in adding new rider!');
-        }
-    };
-
-
-    const renderTableData = () => {
-        getRiders();
-        return riders.map((rider, index) => {
-            const {imię, nazwisko, przydomek, data_urodzenia} = rider
-            return (
-                <tr>
-                    <td className="first-column">{imię}</td>
-                    <td>{nazwisko}</td>
-                    <td>{przydomek}</td>
-                    <td>{data_urodzenia}</td>
-                    <td></td>
-                    <td className="table-X">
-                        <a href="/usun">
-                            X
-                        </a>
-                    </td>
-                </tr>
-            )
-        })
-    }
-
-    const renderTableHeader = () =>{
-        let header = ["Imię", "Nazwisko", "Przydomek", "Data urodzenia", "Klub"];
-        return header.map((key, index) => {
-        return <th key={index}>{key.toUpperCase()}</th>
-        })
-    }
-
-        return (
-            <div>
-                <table id="riders-list">
-                    <tbody>
-                        <tr>{renderTableHeader()}<th className="table-X__header">USUŃ</th></tr>
-                        {renderTableData()}
-                    </tbody>
-                </table>
-            </div>
-        )
-    
-}
-*/
 class RidersList extends Component<{}, {riders}> {
     constructor(props){
     super(props)
         this.state={
-            //Domyślnie tutaj zaciąganie zawodników z bazy
-            riders: [
-                /*{imię: "Tobiasz", nazwisko: "Musielak", przydomek: "Tofik", data_urodzenia: "1-1-1990", klub: "Apator Toruń"},
-                {imię: "Tobiasz", nazwisko: "Musielak", przydomek: "Tofik", data_urodzenia: "1-1-1990", klub: "Apator Toruń"},
-                {imię: "Tobiasz", nazwisko: "Musielak", przydomek: "Tofik", data_urodzenia: "1-1-1990", klub: "Apator Toruń"}
-            */ ]
+            riders: []
         }
     }
 
@@ -121,13 +26,14 @@ class RidersList extends Component<{}, {riders}> {
                 'https://fantasy-league-eti.herokuapp.com/riders',
                 options
             );
+            console.log(data);
             this.setState(() => ({
                 riders: []
             }));
             data.map((rider) => {
                 this.setState({
                     riders: this.state.riders.concat({
-                       // id: 
+                        id: rider._id, 
                         imię: rider.first_name,
                         nazwisko: rider.last_name,
                         przydomek: rider.nickname,
@@ -135,24 +41,22 @@ class RidersList extends Component<{}, {riders}> {
                     })
                 });
             });
-
-            //setAddRiderError(false);
-            //setAddRiderSuccess(true);
         } catch (e) {
-            //setAddRiderSuccess(false);
-            //setAddRiderError(true);
             console.log(e.response);
-            throw new Error('Error in adding new rider!');
+            throw new Error('Error in getting riders!');
         }
     };
 
+    componentDidMount(){
+        this.getRiders()
+    }
+
 
     renderTableData() {
-        this.getRiders();
         return this.state.riders.map((rider, index) => {
-            const {imię, nazwisko, przydomek, data_urodzenia} = rider
+            const {id, imię, nazwisko, przydomek, data_urodzenia} = rider
             return (
-                <tr>
+                <tr key={id}>
                     <td className="first-column">{imię}</td>
                     <td>{nazwisko}</td>
                     <td>{przydomek}</td>
