7404d62f949d0d4215bc0d93d3f1edb609feca09
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-07 09:26:47 +0200

    Created method to checkBadAuthorization

--- src/validation/checkCookies.ts
+++ src/validation/checkCookies.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import Cookies from 'universal-cookie';
+import addNotification from '../utils/addNotification';
+import { Dispatch, SetStateAction } from 'react';
 
 export const checkCookies = (): boolean => {
 	const cookies = new Cookies();
@@ -8,4 +10,19 @@ export const checkCookies = (): boolean => {
 	return true;
 };
 
-export default checkCookies;
+export const checkBadAuthorization = (
+	setLoggedIn: Dispatch<SetStateAction<boolean>>,
+	push: (path: string, state?: {}) => void
+): void => {
+	const title = 'Błąd!';
+	const type = 'danger';
+	const message = 'Sesja wygasła!';
+	const duration = 3000;
+	const cookies = new Cookies();
+	cookies.remove('access_token');
+	addNotification(title, message, type, duration);
+	setTimeout(() => {
+		setLoggedIn(false);
+		push('/login');
+	}, duration);
+};
