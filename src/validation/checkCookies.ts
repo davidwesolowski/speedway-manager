736f671091dfdd348fa1d6a413093da477c44f4d
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-07 09:25:52 +0200

    Changed checkCookies method to named export

--- src/validation/checkCookies.ts
+++ src/validation/checkCookies.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import Cookies from 'universal-cookie';
 
-const checkCookies = (): boolean => {
+export const checkCookies = (): boolean => {
 	const cookies = new Cookies();
 	const access_token = cookies.get('access_token');
 	if (!access_token) return false;
