4f1f626e03e7d4ae4f946b18dd5743d1a56ad7a8
Dominik Kubiaczyk <domino.98@o2.pl>
2020-08-07 14:01:08 +0200

    Added filtering riders by phrase included in their name or last-name

--- src/components/FindRider.tsx
+++ src/components/FindRider.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent, useState, useEffect } from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
+import { Paper, Typography, Divider, TextField } from '@material-ui/core';
+import addNotification from '../utils/addNotification';
 
 interface IRider{
     //id: string;
@@ -17,6 +19,7 @@ const FindRider: FunctionComponent<RouteComponentProps> = ({
 }) => {
 
     const [riders, setRiders] = useState([]);
+    const [phrase, setPhrase] = useState<string>("");
     const getRiders = async () => {
         try {
             const cookies = new Cookies();
@@ -49,22 +52,43 @@ const FindRider: FunctionComponent<RouteComponentProps> = ({
             });
         } catch (e) {
             console.log(e.response);
+            addNotification("Błąd", "Sesja wygasła", "danger", 3000);
+            setTimeout(() => {
+                push("/login")
+            }, 3000);
             throw new Error('Error in getting riders');
         }
     };
 
     const renderTableData = () => {
-        return riders.map((rider, index) => {
-            const {id, first_name, last_name, nickname, date_of_birth} = rider
-            return (
-                <tr key = {id}>
-                    <td>{first_name}</td>
-                    <td>{last_name}</td>
-                    <td>{nickname}</td>
-                    <td>{date_of_birth}</td>
-                </tr>
-            )
-        })
+        if(phrase.length == 0)
+        {
+            return riders.map((rider, index) => {
+                const {id, first_name, last_name, nickname, date_of_birth} = rider
+                return (
+                    <tr key = {id}>
+                        <td>{first_name}</td>
+                        <td>{last_name}</td>
+                        <td>{nickname}</td>
+                        <td>{date_of_birth}</td>
+                    </tr>
+                )
+            })
+        }
+        else
+        {
+            return riders.filter(rider => ((rider.first_name.toUpperCase())+" "+(rider.last_name.toUpperCase())).includes(phrase.toUpperCase())).map((rider, index) => {
+                const {id, first_name, last_name, nickname, date_of_birth} = rider
+                return (
+                    <tr key = {id}>
+                        <td>{first_name}</td>
+                        <td>{last_name}</td>
+                        <td>{nickname}</td>
+                        <td>{date_of_birth}</td>
+                    </tr>
+                )
+            })
+        }
     }
 
     const renderTableHeader = () => {
@@ -74,21 +98,44 @@ const FindRider: FunctionComponent<RouteComponentProps> = ({
         })
     }
 
+    const handleOnChange = () => (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        event.persist();
+        if(event.target) {
+            setPhrase(
+                phrase => event.target.value
+            );
+        };
+    }
+
     useEffect(() => {
         getRiders()
     }, []);
 
     return(
         <>
-        <div>
-            <table id="riders-list">
-                <tbody>
-                    <tr>
-                        {renderTableHeader()}
-                    </tr>
-                    {renderTableData()}
-                </tbody>
-            </table>
+        <div className="find-rider">
+            <div className="find-rider__background"/>
+            <Paper className="find-rider__box">
+                <Typography variant="h2" className="riders__header">
+                    Szukaj zawodnika
+                </Typography>
+                <Divider/>
+                <TextField
+                    label="Szukaj"
+                    value={phrase}
+                    onChange={handleOnChange()}
+                />
+                <table id="riders-list">
+                    <tbody>
+                        <tr>
+                            {renderTableHeader()}
+                        </tr>
+                        {renderTableData()}
+                    </tbody>
+                </table>
+            </Paper>
         </div>
         </>
     );
