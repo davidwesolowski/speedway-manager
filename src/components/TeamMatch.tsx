28e4fa69aeac15e44278a846d3804afbaaf40b0b
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-10 11:59:51 +0200

    Created box with riders and messages to move riders

--- src/components/TeamMatch.tsx
+++ src/components/TeamMatch.tsx
@@ -75,7 +75,115 @@ const intersection = (a: ITempRider[], b: ITempRider[]) => {
 const union = (a: ITempRider[], b: ITempRider[]) => [...a, ...not(b, a)];
 
 const TeamMatch: FunctionComponent = () => {
-	return <div>hello</div>;
+	const [checked, setChecked] = useState<ITempRider[]>([]);
+	const [left, setLeft] = useState<ITempRider[]>(riders);
+	const [right, setRight] = useState<ITempRider[]>([]);
+
+	const leftChecked = intersection(checked, left);
+	const rightChecked = intersection(checked, right);
+
+	const handleToggle = (rider: ITempRider) => () => {
+		const currentIndex = checked.indexOf(rider);
+		const newChecked = [...checked];
+		if (currentIndex === -1) {
+			newChecked.push(rider);
+		} else {
+			newChecked.splice(currentIndex, 1);
+		}
+		setChecked(newChecked);
+	};
+
+	const numberOfChecked = (riders: ITempRider[]) =>
+		intersection(checked, riders).length;
+
+	const handleCheckRight = () => {
+		setRight([...right, ...leftChecked]);
+		setLeft(not(left, leftChecked));
+		setChecked(not(checked, leftChecked));
+	};
+
+	const handleCheckAllLeft = () => {
+		setRight([]);
+		setLeft([...left, ...right]);
+	};
+
+	const handleCheckLeft = () => {
+		setLeft([...left, ...rightChecked]);
+		setRight(not(right, rightChecked));
+		setChecked(not(checked, rightChecked));
+	};
+
+	const customList = (
+		title: ReactNode,
+		riders: ITempRider[],
+		header: string
+	) => (
+		<Card>
+			<CardHeader title={header} />
+			<Divider /> n
+			<List dense component="div" role="list">
+				{riders.map((rider: ITempRider) => {
+					const value = `${rider.firstName} ${rider.lastName}`;
+					const labelId = `transfer-list-all-item-${value}-label`;
+					return (
+						<ListItem
+							key={value}
+							role="listitem"
+							button
+							onClick={handleToggle(rider)}
+						>
+							<ListItemIcon>
+								<Checkbox
+									checked={checked.indexOf(rider) !== -1}
+									tabIndex={-1}
+									disableRipple
+									inputProps={{ 'aria-labelledby': labelId }}
+								/>
+							</ListItemIcon>
+							<ListItemText id={labelId} primary={value} />
+						</ListItem>
+					);
+				})}
+			</List>
+		</Card>
+	);
+
+	return (
+		<Grid container alignItems="center" justify="center" spacing={2}>
+			<Grid item>{customList('Choices', left, 'Cała drużyna')}</Grid>
+			<Grid item>
+				<Grid container direction="column" alignItems="center">
+					<Button
+						variant="outlined"
+						size="small"
+						aria-label="move selected right"
+						onClick={handleCheckRight}
+					>
+						&gt;
+					</Button>
+					<Button
+						variant="outlined"
+						size="small"
+						aria-label="move all left"
+						onClick={handleCheckAllLeft}
+					>
+						≪
+					</Button>
+					<Button
+						variant="outlined"
+						size="small"
+						aria-label="move selected left"
+						onClick={handleCheckLeft}
+					>
+						&lt;
+					</Button>
+				</Grid>
+			</Grid>
+			<Grid item style={{ alignSelf: 'flex-start' }}>
+				{customList('Chosen', right, 'Kadra meczowa')}
+			</Grid>
+		</Grid>
+	);
 };
 
 export default TeamMatch;
