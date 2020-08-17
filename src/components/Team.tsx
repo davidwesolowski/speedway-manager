19a0fa55ceea8f9bfc556570494f4a6bf18a1cd5
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-17 07:47:35 +0200

    Added fetching riders from particular team in Team component

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -24,7 +24,6 @@ import TeamGeneral from './TeamGeneral';
 import { AppContext } from './AppProvider';
 import { checkBadAuthorization } from '../validation/checkCookies';
 import { setUser } from '../actions/userActions';
-import { defaultImageData } from '../utils/handleImgFile';
 import TeamMatch from './TeamMatch';
 
 interface ITabPanelProps {
@@ -37,6 +36,17 @@ interface ITeamState {
 	name: string;
 	logo_url: string;
 	_id: string;
+}
+
+export interface IRider {
+	_id: string;
+	firstName: string;
+	lastName: string;
+	club: string;
+	nationality: string;
+	ksm: number;
+	dateOfBirth: string;
+	age: string;
 }
 
 const defaultTeamState = {
@@ -68,6 +78,7 @@ const a11yProps = (index: any) => ({
 const Team: FunctionComponent<RouteComponentProps> = () => {
 	const [value, setValue] = useState<number>(0);
 	const [team, setTeam] = useState<ITeamState>(defaultTeamState);
+	const [riders, setRiders] = useState<IRider[]>([]);
 	const [updatedTeam, setUpdatedTeam] = useState<boolean>(false);
 	const [loading, setLoading] = useState<boolean>(true);
 	const { push } = useHistory();
@@ -85,17 +96,41 @@ const Team: FunctionComponent<RouteComponentProps> = () => {
 				Authorization: `Bearer ${access_token}`
 			}
 		};
-		const fetchTeam = async () => {
+		const fetchTeamRiders = async (_id: string) => {
 			try {
-				const { data } = await axios.get(
-					'https://fantasy-league-eti.herokuapp.com/teams',
+				const { data: riders } = await axios.get(
+					`https://fantasy-league-eti.herokuapp.com/teams/${_id}/riders`,
 					options
 				);
-				if (data.length && data[0]) {
-					const { name, logo_url, _id } = data[0];
-					setTeam({ name, logo_url, _id });
-				} else {
-					setTeam(defaultTeamState);
+				if (riders.length) {
+					const newRiders = riders.map(({ rider }) => {
+						const dateOfBirth = rider.date_of_birth;
+						const riderAgeYear = new Date(
+							dateOfBirth
+						).getFullYear();
+						const currentYear = new Date().getFullYear();
+						const diffYear = currentYear - riderAgeYear;
+						const age =
+							diffYear <= 21
+								? 'U21'
+								: diffYear <= 23
+								? 'U23'
+								: 'Senior';
+						const nationality = rider.isForeigner
+							? 'Zagraniczny'
+							: 'Krajowy';
+						return {
+							firstName: rider.first_name,
+							lastName: rider.last_name,
+							nationality,
+							dateOfBirth,
+							age,
+							ksm: 0,
+							_id: 0,
+							club: ''
+						};
+					});
+					setRiders(newRiders);
 				}
 			} catch (e) {
 				const {
@@ -104,6 +139,28 @@ const Team: FunctionComponent<RouteComponentProps> = () => {
 				if (data.statusCode == 401) {
 					checkBadAuthorization(setLoggedIn, push);
 				}
+			}
+		};
+		const fetchTeam = async () => {
+			try {
+				const { data } = await axios.get(
+					'https://fantasy-league-eti.herokuapp.com/teams',
+					options
+				);
+				if (data.length && data[0]) {
+					const { name, logo_url, _id } = data[0];
+					fetchTeamRiders(_id);
+					setTeam({ name, logo_url, _id });
+				} else {
+					setTeam(defaultTeamState);
+				}
+			} catch (e) {
+				/*const {
+					response: { data }
+				} = e;
+				if (data.statusCode == 401) {
+					checkBadAuthorization(setLoggedIn, push);
+				}*/
 			}
 		};
 		const fetchUserData = async () => {
@@ -117,9 +174,15 @@ const Team: FunctionComponent<RouteComponentProps> = () => {
 				dispatchUserData(setUser({ username, email, avatar_url }));
 				setLoggedIn(true);
 			} catch (e) {
-				/**/
+				/*const {
+					response: { data }
+				} = e;
+				if (data.statusCode == 401) {
+					checkBadAuthorization(setLoggedIn, push);
+				}*/
 			}
 		};
+
 		fetchTeam();
 		if (!userData.username) fetchUserData();
 		setLoading(false);
@@ -160,6 +223,7 @@ const Team: FunctionComponent<RouteComponentProps> = () => {
 						) : team.name ? (
 							<TeamGeneral
 								team={team}
+								riders={riders}
 								updatedTeam={updatedTeam}
 								setUpdatedTeam={setUpdatedTeam}
 							/>
@@ -171,7 +235,7 @@ const Team: FunctionComponent<RouteComponentProps> = () => {
 						)}
 					</TabPanel>
 					<TabPanel value={value} index={1}>
-						<TeamMatch />
+						<TeamMatch riders={riders} />
 					</TabPanel>
 				</div>
 			</Paper>
