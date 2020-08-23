dc19d99989546a62acf6e45c9498d78a39a3cd86
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-08-23 18:35:35 +0200

    Added header and its styles in Users component

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,7 +1,40 @@
 import React, { FunctionComponent } from 'react';
+import {
+	Paper,
+	Typography,
+	TextField,
+	InputAdornment,
+	Grid
+} from '@material-ui/core';
+import { FiSearch } from 'react-icons/fi';
 
 const Users: FunctionComponent = () => {
-	return <div>Hello from Users</div>;
+	return (
+		<div className="users">
+			<div className="users__background"></div>
+			<Paper className="users__box">
+				<Grid container justify="center" alignItems="center">
+					<Grid item>
+						<Typography variant="h3" className="users__headerText">
+							Znajdź użytkownika
+						</Typography>
+					</Grid>
+					<Grid item>
+						<TextField
+							placeholder="Użytkownik..."
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<FiSearch className="users__searchIcon" />
+									</InputAdornment>
+								)
+							}}
+						/>
+					</Grid>
+				</Grid>
+			</Paper>
+		</div>
+	);
 };
 
 export default Users;
