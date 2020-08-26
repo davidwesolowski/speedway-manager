a41b583558d343844e1cffdc4c2e1cacfe8eb5e6
DominikKubiaczyk <domino.98@o2.pl>
2020-08-26 13:08:24 +0200

    Created selects for choosing riders and text fields to points

--- src/components/AddMatch.tsx
+++ src/components/AddMatch.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
-import { Paper, Typography, Divider, Button, InputLabel, MenuItem, Select } from "@material-ui/core";
+import { Paper, Typography, Divider, Button, InputLabel, MenuItem, Select, TextField } from "@material-ui/core";
 
 interface IRiderPoints{
     id: string;
@@ -19,6 +19,73 @@ interface ITeamPoints{
     rider_8: IRiderPoints;
 }
 
+interface IValidatedPoints{
+    points_1: {
+        message: string;
+        error: boolean;
+    }
+    points_2: {
+        message: string;
+        error: boolean;
+    }
+    points_3: {
+        message: string;
+        error: boolean;
+    }
+    points_4: {
+        message: string;
+        error: boolean;
+    }
+    points_5: {
+        message: string;
+        error: boolean;
+    }
+    points_6: {
+        message: string;
+        error: boolean;
+    }
+    points_7: {
+        message: string;
+        error: boolean;
+    }
+    points_8: {
+        message: string;
+        error: boolean;
+    }
+    points_9: {
+        message: string;
+        error: boolean;
+    }
+    points_10: {
+        message: string;
+        error: boolean;
+    }
+    points_11: {
+        message: string;
+        error: boolean;
+    }
+    points_12: {
+        message: string;
+        error: boolean;
+    }
+    points_13: {
+        message: string;
+        error: boolean;
+    }
+    points_14: {
+        message: string;
+        error: boolean;
+    }
+    points_15: {
+        message: string;
+        error: boolean;
+    }
+    points_16: {
+        message: string;
+        error: boolean;
+    }
+}
+
 const AddMatch: FunctionComponent<RouteComponentProps> = ({
     history: { push }
 }) => {
@@ -27,6 +94,79 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
         id: "",
         points: 0
     }
+
+    const idRider = {
+        id: "Mleko"
+    }
+
+    const defaultValidatedPoints = {
+        points_1: {
+            message: '',
+            error: false
+        },
+        points_2: {
+            message: '',
+            error: false
+        },
+        points_3: {
+            message: '',
+            error: false
+        },
+        points_4: {
+            message: '',
+            error: false
+        },
+        points_5: {
+            message: '',
+            error: false
+        },
+        points_6: {
+            message: '',
+            error: false
+        },
+        points_7: {
+            message: '',
+            error: false
+        },
+        points_8: {
+            message: '',
+            error: false
+        },
+        points_9: {
+            message: '',
+            error: false
+        },
+        points_10: {
+            message: '',
+            error: false
+        },
+        points_11: {
+            message: '',
+            error: false
+        },
+        points_12: {
+            message: '',
+            error: false
+        },
+        points_13: {
+            message: '',
+            error: false
+        },
+        points_14: {
+            message: '',
+            error: false
+        },
+        points_15: {
+            message: '',
+            error: false
+        },
+        points_16: {
+            message: '',
+            error: false
+        }
+    }
+
+    const [validatedPoints, setValidatedPoints] = useState<IValidatedPoints>(defaultValidatedPoints)
 
     const [home, setHome] = useState<ITeamPoints>({
         team_id: "",
@@ -54,6 +194,46 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
 
     const [round, setRound] = useState<number>(0)
 
+    const handleOnChangeSelectRider = (number: string, homeAway: string) => (
+        event
+    ) => {
+        event.persist();
+        if(event.target) {
+            if(homeAway === 'home'){
+                setHome((prevState: ITeamPoints) => ({
+                    ...prevState,
+                    [number]: {id: event.target.value}
+                }));
+            }
+            else{
+                setAway((prevState: ITeamPoints) => ({
+                    ...prevState,
+                    [number]: {id: event.target.value}
+                }));
+            }
+        };
+    }
+
+    const handleOnChangePoints = (rider: string, homeAway: string) => (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        event.persist();
+        if(event.target) {
+            if(homeAway === 'home'){
+                setHome((prevState: ITeamPoints) => ({
+                    ...prevState,
+                    [rider]: {points: event.target.value}
+                }));
+            }
+            else{
+                setAway((prevState: ITeamPoints) => ({
+                    ...prevState,
+                    [rider]: {points: event.target.value}
+                }));
+            }
+        };
+    }
+
     return(
         <>
             <div className="add-match">
@@ -66,7 +246,7 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
                     <br/>
                     <div className="add-match__round-div">
                         <InputLabel id="roundLabel">Kolejka:</InputLabel>
-                        <Select labelId="roundLabel" className="add-match__round-select" value={round}>
+                        <Select labelId="roundLabel" className="add-match__round-select" value={round} >
                             <MenuItem value="New">Dodaj nową kolejkę</MenuItem>
                         </Select>
                     </div>
@@ -77,6 +257,63 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
                         <Select className="add-match__team-select" value={away.team_id}>
                             <MenuItem value="Away">Przyjezdni</MenuItem>
                         </Select>
+                        <br/>
+                        <div className="add-match__rider-div">
+                            1.
+                            <Select className="add-match__rider-select" value={away.rider_1.id} onChange={handleOnChangeSelectRider('rider_1', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_1.points} onChange={handleOnChangePoints('rider_1', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            2.
+                            <Select className="add-match__rider-select" value={away.rider_2.id} onChange={handleOnChangeSelectRider('rider_2', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_2.points} onChange={handleOnChangePoints('rider_2', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            3.
+                            <Select className="add-match__rider-select" value={away.rider_3.id} onChange={handleOnChangeSelectRider('rider_3', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_3.points} onChange={handleOnChangePoints('rider_3', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            4.
+                            <Select className="add-match__rider-select" value={away.rider_4.id} onChange={handleOnChangeSelectRider('rider_4', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_4.points} onChange={handleOnChangePoints('rider_4', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            5.
+                            <Select className="add-match__rider-select" value={away.rider_5.id} onChange={handleOnChangeSelectRider('rider_5', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_5.points} onChange={handleOnChangePoints('rider_5', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            6.
+                            <Select className="add-match__rider-select" value={away.rider_6.id} onChange={handleOnChangeSelectRider('rider_6', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_6.points} onChange={handleOnChangePoints('rider_6', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            7.
+                            <Select className="add-match__rider-select" value={away.rider_7.id} onChange={handleOnChangeSelectRider('rider_7', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_7.points} onChange={handleOnChangePoints('rider_7', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            8.
+                            <Select className="add-match__rider-select" value={away.rider_8.id} onChange={handleOnChangeSelectRider('rider_8', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_8.points} onChange={handleOnChangePoints('rider_8', 'away')}/>
+                        </div>
                     </div>
                     <div className="add-match__home-div">
                         HOME
@@ -84,9 +321,66 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
                         <Select className="add-match__team-select" value={home.team_id}>
                             <MenuItem value="Home">Gospodarze</MenuItem>
                         </Select>
+                        <br/>
+                        <div className="add-match__rider-div">
+                            9.
+                            <Select className="add-match__rider-select" value={away.rider_1.id} onChange={handleOnChangeSelectRider('rider_1', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_1.points} onChange={handleOnChangePoints('rider_1', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            10.
+                            <Select className="add-match__rider-select" value={away.rider_2.id} onChange={handleOnChangeSelectRider('rider_2', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_2.points} onChange={handleOnChangePoints('rider_2', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            11.
+                            <Select className="add-match__rider-select" value={away.rider_3.id} onChange={handleOnChangeSelectRider('rider_3', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_3.points} onChange={handleOnChangePoints('rider_3', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            12.
+                            <Select className="add-match__rider-select" value={away.rider_4.id} onChange={handleOnChangeSelectRider('rider_4', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_4.points} onChange={handleOnChangePoints('rider_4', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            13.
+                            <Select className="add-match__rider-select" value={away.rider_5.id} onChange={handleOnChangeSelectRider('rider_5', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_5.points} onChange={handleOnChangePoints('rider_5', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            14.
+                            <Select className="add-match__rider-select" value={away.rider_6.id} onChange={handleOnChangeSelectRider('rider_6', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_6.points} onChange={handleOnChangePoints('rider_6', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            15.
+                            <Select className="add-match__rider-select" value={away.rider_7.id} onChange={handleOnChangeSelectRider('rider_7', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_7.points} onChange={handleOnChangePoints('rider_7', 'away')}/>
+                        </div>
+                        <div className="add-match__rider-div">
+                            16.
+                            <Select className="add-match__rider-select" value={away.rider_8.id} onChange={handleOnChangeSelectRider('rider_8', 'away')}>
+                                <MenuItem value="rider">Zawodnik</MenuItem>
+                            </Select>
+                            <TextField className="add-match__rider-points" value={away.rider_8.points} onChange={handleOnChangePoints('rider_8', 'away')}/>
+                        </div>
                     </div>
                     <br/>
-                    <Button>
+                    <Button className="add-match__submit-button">
                         Dodaj
                     </Button>
                 </Paper>
