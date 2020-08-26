85249c18550c3a05760bee3cabdc5588eee5a382
DominikKubiaczyk <domino.98@o2.pl>
2020-08-26 14:08:53 +0200

    Created dialog to adding new rounds

--- src/components/AddMatch.tsx
+++ src/components/AddMatch.tsx
@@ -1,10 +1,19 @@
 import React, { FunctionComponent, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
-import { Paper, Typography, Divider, Button, InputLabel, MenuItem, Select, TextField } from "@material-ui/core";
+import { Paper, Typography, Divider, Button, InputLabel, MenuItem, Select, TextField, Dialog, DialogTitle, IconButton, DialogContent, Grid, FormControl } from "@material-ui/core";
+import { FiX } from "react-icons/fi";
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 interface IRiderPoints{
     id: string;
     points: number;
+}
+
+interface IRoundCreate{
+    round: number;
+    date_of_begin: Date;
+    date_of_end: Date;
 }
 
 interface ITeamPoints{
@@ -86,6 +95,21 @@ interface IValidatedPoints{
     }
 }
 
+interface IValidatedRound{
+    round: {
+        message: string;
+        error: boolean;
+    }
+    date_of_begin: {
+        message: string;
+        error: boolean;
+    }
+    date_of_end: {
+        message: string;
+        error: boolean;
+    }
+}
+
 const AddMatch: FunctionComponent<RouteComponentProps> = ({
     history: { push }
 }) => {
@@ -95,9 +119,13 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
         points: 0
     }
 
-    const idRider = {
-        id: "Mleko"
+    const defaultRoundCreate = {
+        round: 0,
+        date_of_begin: new Date(),
+        date_of_end: new Date()
     }
+
+    const [roundCreate, setRoundCreate] = useState<IRoundCreate>(defaultRoundCreate)
 
     const defaultValidatedPoints = {
         points_1: {
@@ -166,6 +194,23 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
         }
     }
 
+    const defaultValidatedRound = {
+        round: {
+            message: '',
+            error: false
+        },
+        date_of_begin: {
+            message: '',
+            error: false
+        },
+        date_of_end: {
+            message: '',
+            error: false
+        }
+    }
+
+    const [validatedRound, setValidatedRound] = useState<IValidatedRound>(defaultValidatedRound)
+
     const [validatedPoints, setValidatedPoints] = useState<IValidatedPoints>(defaultValidatedPoints)
 
     const [home, setHome] = useState<ITeamPoints>({
@@ -192,6 +237,17 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
         rider_8: defaultRiderPoints
     })
 
+    const [addRoundDialogOpen, setAddRoundDialogOpen] = useState<boolean>(false);
+
+    const handleOpen = () => {
+        setAddRoundDialogOpen(true);
+    }
+
+    const handleClose = () => {
+        setAddRoundDialogOpen(false);
+        setRoundCreate(defaultRoundCreate);
+    }
+
     const [round, setRound] = useState<number>(0)
 
     const handleOnChangeSelectRider = (number: string, homeAway: string) => (
@@ -210,6 +266,17 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
                     ...prevState,
                     [number]: {id: event.target.value}
                 }));
+            }
+        };
+    }
+
+    const handleOnChangeSelectRound = () => (
+        event
+    ) => {
+        event.persist();
+        if(event.target) {
+            if(event.target.value == "New"){
+                handleOpen();
             }
         };
     }
@@ -234,6 +301,44 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
         };
     }
 
+    const handleOnChangeRoundForm = () => (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        event.persist();
+        if(event.target) {
+            if(event.target.value === ""){
+                setRoundCreate((prevState: IRoundCreate) => ({
+                    ...prevState,
+                    round: 0
+                }));
+            }
+            else{
+            setRoundCreate((prevState: IRoundCreate) => ({
+                ...prevState,
+                round: parseInt(event.target.value)
+            }));
+        }
+        };
+    }
+
+    const handleDateBeginOnChange = date => {
+        setRoundCreate((prevState: IRoundCreate) => ({
+            ...prevState,
+            date_of_begin: date
+        }));
+    };
+
+    const handleDateEndOnChange = date => {
+        setRoundCreate((prevState: IRoundCreate) => ({
+            ...prevState,
+            date_of_end: date
+        }));
+    };
+
+    const handleOnSubmitRound = () => {
+
+    }
+
     return(
         <>
             <div className="add-match">
@@ -246,10 +351,73 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
                     <br/>
                     <div className="add-match__round-div">
                         <InputLabel id="roundLabel">Kolejka:</InputLabel>
-                        <Select labelId="roundLabel" className="add-match__round-select" value={round} >
+                        <Select labelId="roundLabel" className="add-match__round-select" value={round} onChange={handleOnChangeSelectRound()}>
                             <MenuItem value="New">Dodaj nową kolejkę</MenuItem>
                         </Select>
                     </div>
+                    <Dialog open={addRoundDialogOpen} onClose={handleClose} className="round-dialog">
+                        <DialogTitle>
+                            <div className="round-dialog__header">
+                                <Typography variant="h4" className="round-dialog__title">
+                                    Dodawanie kolejki
+                                </Typography>
+                                <IconButton onClick={handleClose} className="round-dialog__fix">
+                                    <FiX />
+                                </IconButton>
+                            </div>
+                        </DialogTitle>
+                        <DialogContent dividers>
+                            <form className="round-dialog__form" onSubmit={handleOnSubmitRound}>
+                                <Grid container>
+                                    <Grid item xs={7} className="round-dialog__form-fields">
+                                        <FormControl className="round-dialog__form-field">
+                                            <TextField
+                                                label="Numer kolejki"
+                                                required
+                                                autoComplete="round"
+                                                value={roundCreate.round}
+                                                error={validatedRound.round.error}
+                                                helperText={validatedRound.round.message}
+                                                onChange={handleOnChangeRoundForm()}
+                                            />
+                                        </FormControl>
+                                        <br/>
+                                        <FormControl className="round-dialog__form-field-date">
+                                            Data rozpoczęcia kolejki:
+                                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                                <KeyboardDatePicker
+                                                    margin="normal"
+                                                    id="date-picker-dialog-1"
+                                                    format="dd/MM/yyyy"
+                                                    value={roundCreate.date_of_begin}
+                                                    onChange={handleDateBeginOnChange}
+                                                    KeyboardButtonProps={{
+                                                        'aria-label': 'change date'
+                                                    }}
+                                                />
+                                            </MuiPickersUtilsProvider>
+                                        </FormControl>
+                                        <br/>
+                                        <FormControl className="round-dialog__form-field-date">
+                                            Data zakończenia kolejki:
+                                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                                <KeyboardDatePicker
+                                                    margin="normal"
+                                                    id="date-picker-dialog-2"
+                                                    format="dd/MM/yyyy"
+                                                    value={roundCreate.date_of_end}
+                                                    onChange={handleDateEndOnChange}
+                                                    KeyboardButtonProps={{
+                                                        'aria-label': 'change date'
+                                                    }}
+                                                />
+                                            </MuiPickersUtilsProvider>
+                                        </FormControl>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
                     <br/>
                     <div className="add-match__away-div">
                         AWAY
@@ -324,59 +492,59 @@ const AddMatch: FunctionComponent<RouteComponentProps> = ({
                         <br/>
                         <div className="add-match__rider-div">
                             9.
-                            <Select className="add-match__rider-select" value={away.rider_1.id} onChange={handleOnChangeSelectRider('rider_1', 'away')}>
+                            <Select className="add-match__rider-select" value={home.rider_1.id} onChange={handleOnChangeSelectRider('rider_1', 'home')}>
                                 <MenuItem value="rider">Zawodnik</MenuItem>
                             </Select>
-                            <TextField className="add-match__rider-points" value={away.rider_1.points} onChange={handleOnChangePoints('rider_1', 'away')}/>
+                            <TextField className="add-match__rider-points" value={home.rider_1.points} onChange={handleOnChangePoints('rider_1', 'home')}/>
                         </div>
                         <div className="add-match__rider-div">
                             10.
-                            <Select className="add-match__rider-select" value={away.rider_2.id} onChange={handleOnChangeSelectRider('rider_2', 'away')}>
+                            <Select className="add-match__rider-select" value={home.rider_2.id} onChange={handleOnChangeSelectRider('rider_2', 'home')}>
                                 <MenuItem value="rider">Zawodnik</MenuItem>
                             </Select>
-                            <TextField className="add-match__rider-points" value={away.rider_2.points} onChange={handleOnChangePoints('rider_2', 'away')}/>
+                            <TextField className="add-match__rider-points" value={home.rider_2.points} onChange={handleOnChangePoints('rider_2', 'home')}/>
                         </div>
                         <div className="add-match__rider-div">
                             11.
-                            <Select className="add-match__rider-select" value={away.rider_3.id} onChange={handleOnChangeSelectRider('rider_3', 'away')}>
+                            <Select className="add-match__rider-select" value={home.rider_3.id} onChange={handleOnChangeSelectRider('rider_3', 'home')}>
                                 <MenuItem value="rider">Zawodnik</MenuItem>
                             </Select>
-                            <TextField className="add-match__rider-points" value={away.rider_3.points} onChange={handleOnChangePoints('rider_3', 'away')}/>
+                            <TextField className="add-match__rider-points" value={home.rider_3.points} onChange={handleOnChangePoints('rider_3', 'home')}/>
                         </div>
                         <div className="add-match__rider-div">
                             12.
-                            <Select className="add-match__rider-select" value={away.rider_4.id} onChange={handleOnChangeSelectRider('rider_4', 'away')}>
+                            <Select className="add-match__rider-select" value={home.rider_4.id} onChange={handleOnChangeSelectRider('rider_4', 'home')}>
                                 <MenuItem value="rider">Zawodnik</MenuItem>
                             </Select>
-                            <TextField className="add-match__rider-points" value={away.rider_4.points} onChange={handleOnChangePoints('rider_4', 'away')}/>
+                            <TextField className="add-match__rider-points" value={home.rider_4.points} onChange={handleOnChangePoints('rider_4', 'home')}/>
                         </div>
                         <div className="add-match__rider-div">
                             13.
-                            <Select className="add-match__rider-select" value={away.rider_5.id} onChange={handleOnChangeSelectRider('rider_5', 'away')}>
+                            <Select className="add-match__rider-select" value={home.rider_5.id} onChange={handleOnChangeSelectRider('rider_5', 'home')}>
                                 <MenuItem value="rider">Zawodnik</MenuItem>
                             </Select>
-                            <TextField className="add-match__rider-points" value={away.rider_5.points} onChange={handleOnChangePoints('rider_5', 'away')}/>
+                            <TextField className="add-match__rider-points" value={home.rider_5.points} onChange={handleOnChangePoints('rider_5', 'home')}/>
                         </div>
                         <div className="add-match__rider-div">
                             14.
-                            <Select className="add-match__rider-select" value={away.rider_6.id} onChange={handleOnChangeSelectRider('rider_6', 'away')}>
+                            <Select className="add-match__rider-select" value={home.rider_6.id} onChange={handleOnChangeSelectRider('rider_6', 'home')}>
                                 <MenuItem value="rider">Zawodnik</MenuItem>
                             </Select>
-                            <TextField className="add-match__rider-points" value={away.rider_6.points} onChange={handleOnChangePoints('rider_6', 'away')}/>
+                            <TextField className="add-match__rider-points" value={home.rider_6.points} onChange={handleOnChangePoints('rider_6', 'home')}/>
                         </div>
                         <div className="add-match__rider-div">
                             15.
-                            <Select className="add-match__rider-select" value={away.rider_7.id} onChange={handleOnChangeSelectRider('rider_7', 'away')}>
+                            <Select className="add-match__rider-select" value={home.rider_7.id} onChange={handleOnChangeSelectRider('rider_7', 'home')}>
                                 <MenuItem value="rider">Zawodnik</MenuItem>
                             </Select>
-                            <TextField className="add-match__rider-points" value={away.rider_7.points} onChange={handleOnChangePoints('rider_7', 'away')}/>
+                            <TextField className="add-match__rider-points" value={home.rider_7.points} onChange={handleOnChangePoints('rider_7', 'home')}/>
                         </div>
                         <div className="add-match__rider-div">
                             16.
-                            <Select className="add-match__rider-select" value={away.rider_8.id} onChange={handleOnChangeSelectRider('rider_8', 'away')}>
+                            <Select className="add-match__rider-select" value={home.rider_8.id} onChange={handleOnChangeSelectRider('rider_8', 'home')}>
                                 <MenuItem value="rider">Zawodnik</MenuItem>
                             </Select>
-                            <TextField className="add-match__rider-points" value={away.rider_8.points} onChange={handleOnChangePoints('rider_8', 'away')}/>
+                            <TextField className="add-match__rider-points" value={home.rider_8.points} onChange={handleOnChangePoints('rider_8', 'home')}/>
                         </div>
                     </div>
                     <br/>
