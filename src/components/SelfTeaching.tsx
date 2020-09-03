4f69cda1762a4260943f87653f626d3f4d1e938a
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-09-03 18:05:00 +0200

    Added plus button to show dialog to add faq

--- src/components/SelfTeaching.tsx
+++ src/components/SelfTeaching.tsx
@@ -1,14 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, FunctionComponent } from 'react';
 import axios from 'axios';
 import { useStateValue } from './AppProvider';
 import { checkCookies, checkBadAuthorization } from '../utils/checkCookies';
 import getToken from '../utils/getToken';
 import { setUser } from '../actions/userActions';
 import { useHistory } from 'react-router-dom';
+import {
+	Paper,
+	Grid,
+	IconButton,
+	Dialog,
+	DialogTitle,
+	Typography,
+	DialogContent
+} from '@material-ui/core';
+import { FaPlusCircle } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 
-const SelfTeaching = () => {
+const SelfTeaching: FunctionComponent = () => {
+	const [open, setOpen] = useState(false);
 	const { userData, dispatchUserData, setLoggedIn } = useStateValue();
 	const { push } = useHistory();
+
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
 	useEffect(() => {
 		const checkIfUserLoggedIn = async () => {
@@ -43,9 +58,40 @@ const SelfTeaching = () => {
 	}, []);
 
 	return (
-		<div className="selfTeaching__container">
-			<div className="selfTeaching__img"></div>
-		</div>
+		<>
+			<div className="selfTeaching__container">
+				<div className="selfTeaching__img"></div>
+				<Paper className="selfTeaching__box">
+					<Grid container justify="flex-end" alignItems="center">
+						<Grid item>
+							<IconButton onClick={handleOpen}>
+								<FaPlusCircle className="selfTeaching__icon" />
+							</IconButton>
+						</Grid>
+					</Grid>
+				</Paper>
+			</div>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				className="selfTeaching__dialog"
+			>
+				<DialogTitle>
+					<Grid container justify="space-between" alignItems="center">
+						<Typography
+							variant="h4"
+							className="selfTeaching__dialogHeader"
+						>
+							Dodaj pytanie do samouczka
+						</Typography>
+						<IconButton>
+							<FiX className="selfTeaching__icon" />
+						</IconButton>
+					</Grid>
+				</DialogTitle>
+				<DialogContent dividers></DialogContent>
+			</Dialog>
+		</>
 	);
 };
 
