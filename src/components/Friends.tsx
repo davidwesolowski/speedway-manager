4386d4213b4e87c23c2b66ae35a66c726185a7e2
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-09-11 00:04:39 +0200

    Added container to list friends

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -1,8 +1,19 @@
 import React, { FunctionComponent } from 'react';
 import { RouteProps } from 'react-router-dom';
+import { Paper, Typography, Divider } from '@material-ui/core';
 
 const Friends: FunctionComponent<RouteProps> = () => {
-	return <div>Hello</div>;
+	return (
+		<div className="friends">
+			<div className="friends__background"></div>
+			<Paper className="friends__box">
+				<Typography variant="h2" className="friends__headerText">
+					Lista znajomych
+				</Typography>
+				<Divider />
+			</Paper>
+		</div>
+	);
 };
 
 export default Friends;
