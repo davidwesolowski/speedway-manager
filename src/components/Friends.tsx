2abf24f43cc0a07602b8929482441bd607c865ad
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-09-11 16:53:20 +0200

    Added fetching userData, pending invitations, current friends and teams

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -1,8 +1,118 @@
-import React, { FunctionComponent } from 'react';
-import { RouteProps } from 'react-router-dom';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { RouteProps, useHistory } from 'react-router-dom';
+import axios from 'axios';
 import { Paper, Typography, Divider } from '@material-ui/core';
+import getToken from '../utils/getToken';
+import { useStateValue } from './AppProvider';
+import { setUser } from '../actions/userActions';
+import { checkBadAuthorization } from '../utils/checkCookies';
+import { IUsers } from './Users';
 
 const Friends: FunctionComponent<RouteProps> = () => {
+	const [friends, setFriends] = useState<IUsers[]>([]);
+	const { setLoggedIn, dispatchUserData, userData } = useStateValue();
+	const { push } = useHistory();
+
+	useEffect(() => {
+		const accessToken = getToken();
+		const options = {
+			headers: {
+				Authorization: `Bearer ${accessToken}`
+			}
+		};
+		const fetchUserData = async () => {
+			try {
+				const {
+					data: { _id, username, email, avatarUrl }
+				} = await axios.get(
+					'https://fantasy-league-eti.herokuapp.com/users/self',
+					options
+				);
+				dispatchUserData(setUser({ _id, username, email, avatarUrl }));
+				setLoggedIn(true);
+				return _id;
+			} catch (e) {
+				const {
+					response: { data }
+				} = e;
+				if (data.statusCoed == 401) {
+					checkBadAuthorization(setLoggedIn, push);
+				}
+			}
+		};
+
+		const fetchCurrentAndPendingFriends = async () => {
+			try {
+				const userId = await fetchUserData();
+				if (userId) {
+					const { data: currentFriends } = await axios.get(
+						`https://fantasy-league-eti.herokuapp.com/friendlist/myFriends/${userId}`,
+						options
+					);
+					const { data: pendingFriends } = await axios.get(
+						`https://fantasy-league-eti.herokuapp.com/friendlist/pendingInvitations/${userId}`,
+						options
+					);
+					const { data: teams } = await axios.get(
+						'https://fantasy-league-eti.herokuapp.com/teams/all',
+						options
+					);
+					const allFriends = [...currentFriends, ...pendingFriends];
+					const friendsWithPersonalDetails = await Promise.all(
+						allFriends.map(async friend => {
+							let _id;
+							if (friend.invitedId != userId) {
+								_id = friend.invitedId;
+							} else if (friend.senderId != userId) {
+								_id = friend.senderId;
+							}
+							const { data: user } = await axios.get(
+								`https://fantasy-league-eti.herokuapp.com/users/${_id}`,
+								options
+							);
+							return user;
+						})
+					);
+					const friendsWithTeamDetails = friendsWithPersonalDetails.map(
+						friend => {
+							const team = teams.find(
+								team => team.userId == friend._id
+							);
+							if (team) {
+								return {
+									...friend,
+									teamName: team.name,
+									teamLogo: team.logoUrl,
+									teamId: team._id
+								};
+							}
+							return {
+								...friend,
+								teamName: null,
+								teamLogo: null,
+								teamId: null
+							};
+						}
+					);
+					setFriends(friendsWithTeamDetails);
+				}
+			} catch (e) {
+				const {
+					response: { data }
+				} = e;
+				if (data.statusCode == 401) {
+					checkBadAuthorization(setLoggedIn, push);
+				}
+			}
+		};
+
+		fetchCurrentAndPendingFriends();
+
+		setTimeout(() => {
+			document.body.style.overflow = 'auto';
+		}, 3000);
+	}, []);
+
 	return (
 		<div className="friends">
 			<div className="friends__background"></div>
