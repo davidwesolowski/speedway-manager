800cf2845186388adc3edf6e436340e2f9f9ce92
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-09-18 14:54:12 +0200

    Created ClubLeagueCreate componenet and styles for it

---
+++ src/components/ClubLeagueCreate.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const ClubLeagueCreate: FunctionComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ClubLeagueCreate;
