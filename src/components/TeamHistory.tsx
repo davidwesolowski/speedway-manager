7b5058d7213e20e0693db27913fd2333b237e666
DominikKubiaczyk <domino.98@o2.pl>
2020-09-22 14:37:26 +0200

    Implemented table of riders and total score view in team history

--- src/components/TeamHistory.tsx
+++ src/components/TeamHistory.tsx
@@ -1,4 +1,4 @@
-import { Divider, MenuItem, Paper, Select, Typography } from '@material-ui/core';
+import { Divider, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core';
 import axios from 'axios';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
@@ -44,6 +44,35 @@ const TeamHistory : FunctionComponent<RouteComponentProps> = ({history: { push }
 
     const [rounds, setRounds] = useState([]);
     const [selectedRound, setSelectedRound] = useState<string>('all');
+    const [historyRiders, setHistoryRiders] = useState([]);
+    const [updatedRiders, setUpdatedRiders] = useState<boolean>(true);
+
+    const [tempHistoryRiders, setTempHistoryRiders] = useState([
+        {
+            _id: 1,
+            firstName: "Bartosz",
+            lastName: "Zmarzlik",
+            club: "Stal Gorzów",
+            KSM: 10,
+            score: 15
+        },
+        {
+            _id: 2,
+            firstName: "Bartosz",
+            lastName: "Zmarzlik",
+            club: "Stal Gorzów",
+            KSM: 10,
+            score: 14
+        },
+        {
+            _id: 3,
+            firstName: "Bartosz",
+            lastName: "Zmarzlik",
+            club: "Stal Gorzów",
+            KSM: 10,
+            score: 13
+        }
+    ])
 
     const getRounds = async () => {
         try{
@@ -77,6 +106,42 @@ const TeamHistory : FunctionComponent<RouteComponentProps> = ({history: { push }
         }
     }
 
+    const getHistoryRiders = async () => {
+        setUpdatedRiders(false);
+
+        /*try{
+            const accessToken = getToken();
+            const options = {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            };
+            const {data} = await axios.get(
+                'https://fantasy-league-eti.herokuapp.com/history-riders',
+                options
+            );
+
+            setHistoryRiders(data);
+        } catch (e) {
+            const {
+                response: { data }
+            } = e;
+            if (data.statusCode == 401) {
+                checkBadAuthorization(setLoggedIn, push);
+            } else {
+                addNotification(
+                    'Błąd',
+                    'Nie udało się pobrać rund z bazy',
+                    'danger',
+                    3000
+                );
+            }
+            throw new Error('Error in getting rounds');
+        }*/
+
+        setUpdatedRiders(true);
+    }
+
     const generateRounds = () => {
         return rounds.map((round, index) => {
             return(
@@ -89,6 +154,43 @@ const TeamHistory : FunctionComponent<RouteComponentProps> = ({history: { push }
         event.persist();
         if(event.target){
             setSelectedRound(event.target.value);
+        }
+    }
+
+    const generateTable = () => {
+        if(updatedRiders){
+            return(
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell>Imię</TableCell>
+                                <TableCell>Nazwisko</TableCell>
+                                <TableCell>KSM</TableCell>
+                                <TableCell>Klub</TableCell>
+                                <TableCell>Wynik</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {tempHistoryRiders.map(rider => (
+                                <TableRow key={rider._id} hover={true}>
+                                    <TableCell></TableCell>
+                                    <TableCell>{rider.firstName}</TableCell>
+                                    <TableCell>{rider.lastName}</TableCell>
+                                    <TableCell>{rider.KSM}</TableCell>
+                                    <TableCell>{rider.club}</TableCell>
+                                    <TableCell>{rider.score}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            ) 
+        } else {
+            return(
+                <div></div>
+            )
         }
     }
 
@@ -116,10 +218,12 @@ const TeamHistory : FunctionComponent<RouteComponentProps> = ({history: { push }
                     </Select>
                     <br/>
                     <div className='history__riders-list'>
-
+                        {generateTable()}
                     </div>
                     <div className='history__full-score'>
                         <Typography variant='h1' className='history__full-score-text'>
+                            Łączny wynik:
+                            <br/>
                             69
                         </Typography>
                     </div>
