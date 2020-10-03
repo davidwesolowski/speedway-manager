94da770a29cae27cbe2837a485989c5916575112
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-10-04 00:12:39 +0200

    Added checkBadAuth method to ListMatches* components

--- src/components/Leagues.tsx
+++ src/components/Leagues.tsx
@@ -1,5 +1,19 @@
 import { ValidationErrorItem } from '@hapi/joi';
-import { Button, Dialog, DialogContent, DialogTitle, Divider, FormControl, Grid, IconButton, MenuItem, Paper, Select, TextField, Typography } from '@material-ui/core';
+import {
+	Button,
+	Dialog,
+	DialogContent,
+	DialogTitle,
+	Divider,
+	FormControl,
+	Grid,
+	IconButton,
+	MenuItem,
+	Paper,
+	Select,
+	TextField,
+	Typography
+} from '@material-ui/core';
 import axios from 'axios';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { FiPlus, FiX, FiXCircle } from 'react-icons/fi';
@@ -11,96 +25,95 @@ import getToken from '../utils/getToken';
 import validateLeagueData from '../validation/validateLeagueData';
 import { useStateValue } from './AppProvider';
 
-interface IValidatedData{
-    name: {
-        message: string;
-        error: boolean;
-    };
-    country: {
-        message: string;
-        error: boolean;
-    }
+interface IValidatedData {
+	name: {
+		message: string;
+		error: boolean;
+	};
+	country: {
+		message: string;
+		error: boolean;
+	};
 }
 
-const Leagues: FunctionComponent<RouteComponentProps> = ({history: { push }}) => {
+const Leagues: FunctionComponent<RouteComponentProps> = ({
+	history: { push }
+}) => {
+	const { setLoggedIn, dispatchUserData, userData } = useStateValue();
 
-    const {
-        setLoggedIn,
-		dispatchUserData,
-		userData,
-    } = useStateValue();
-
-    const fetchUserData = async () => {
-        const accessToken = getToken();
+	const fetchUserData = async () => {
+		const accessToken = getToken();
 		const options = {
 			headers: {
 				Authorization: `Bearer ${accessToken}`
 			}
 		};
-        try {
-            const {
-                data: { username, email, avatarUrl }
-            } = await axios.get(
-                'https://fantasy-league-eti.herokuapp.com/users/self',
-                options
-            );
-            dispatchUserData(setUser({ username, email, avatarUrl }));
-            setLoggedIn(true);
-        } catch (e) {
-            const {
-                response: { data }
-            } = e;
-            if (data.statusCode == 401) {
-                checkBadAuthorization(setLoggedIn, push);
-            }
-        }
-    };
+		try {
+			const {
+				data: { username, email, avatarUrl }
+			} = await axios.get(
+				'https://fantasy-league-eti.herokuapp.com/users/self',
+				options
+			);
+			dispatchUserData(setUser({ username, email, avatarUrl }));
+			setLoggedIn(true);
+		} catch (e) {
+			const {
+				response: { data }
+			} = e;
+			if (data.statusCode == 401) {
+				checkBadAuthorization(setLoggedIn, push);
+			}
+		}
+	};
 
-    const [leagues, setLeagues] = useState([]);
-    const [addLeagueName, setAddLeagueName] = useState<string>('');
-    const [addLeagueCountry, setAddLeagueCountry] = useState<string>('Polska');
-    const [openDialog, setOpenDialog] = useState<boolean>(false);
+	const [leagues, setLeagues] = useState([]);
+	const [addLeagueName, setAddLeagueName] = useState<string>('');
+	const [addLeagueCountry, setAddLeagueCountry] = useState<string>('Polska');
+	const [openDialog, setOpenDialog] = useState<boolean>(false);
 
-    const defaultValidatedData = {
-        name: {
-            message: '',
-            error: false
-        },
-        country: {
-            message: '',
-            error: false
-        }
-    }
+	const defaultValidatedData = {
+		name: {
+			message: '',
+			error: false
+		},
+		country: {
+			message: '',
+			error: false
+		}
+	};
 
-    const [validatedData, setValidatedData] = useState<IValidatedData>(defaultValidatedData);
+	const [validatedData, setValidatedData] = useState<IValidatedData>(
+		defaultValidatedData
+	);
 
-    const handleOpenDialog = () => {
-        setOpenDialog(true)
-    }
+	const handleOpenDialog = () => {
+		setOpenDialog(true);
+	};
 
-    const handleCloseDialog = () => {
-        setOpenDialog(false);
-        setAddLeagueCountry('Polska');
-        setAddLeagueName('');
-    }
+	const handleCloseDialog = () => {
+		setOpenDialog(false);
+		setAddLeagueCountry('Polska');
+		setAddLeagueName('');
+	};
 
-    const [tempLeagues, setTempLeagues] = useState([
-        {
-            name: 'PGE Ekstraliga',
-            country: "Polska"
-        },
-        {
-            name: 'eWinner 1 liga żużlowa',
-            country: 'Polska'
-        },
-        {
-            name: '2 liga żużlowa',
-            country: 'Polska'
-        }
-    ])
+	const [tempLeagues, setTempLeagues] = useState([
+		{
+			name: 'PGE Ekstraliga',
+			country: 'Polska'
+		},
+		{
+			name: 'eWinner 1 liga żużlowa',
+			country: 'Polska'
+		},
+		{
+			name: '2 liga żużlowa',
+			country: 'Polska'
+		}
+	]);
 
-    const getLeagues = async () => {
-        /*try {
+	const getLeagues = async () => {
+		/*try {
             const accessToken = getToken();
             const options = {
                 headers: {
@@ -130,10 +143,10 @@ const Leagues: FunctionComponent<RouteComponentProps> = ({history: { push }}) =>
             }
             throw new Error('Error in getting leagues');
         }*/
-    }
+	};
 
-    const addLeague = async () => {
-        /*try {
+	const addLeague = async () => {
+		/*try {
             const accessToken = getToken();
             const options = {
                 headers: {
@@ -165,10 +178,10 @@ const Leagues: FunctionComponent<RouteComponentProps> = ({history: { push }}) =>
             }
             throw new Error('Error in adding league');
         }*/
-    }
+	};
 
-    const deleteLeague = async (id) => {
-        /*try {
+	const deleteLeague = async id => {
+		/*try {
 			const accessToken = getToken();
 			const options = {
 				headers: {
@@ -198,12 +211,12 @@ const Leagues: FunctionComponent<RouteComponentProps> = ({history: { push }}) =>
 			console.log(e.response);
 			throw new Error('Error in deleting league!');
 		}*/
-    }
+	};
 
-    const renderTableData = () => {
-        return tempLeagues.map((league, index) => {
-            return(
-                <tr
+	const renderTableData = () => {
+		return tempLeagues.map((league, index) => {
+			return (
+				<tr
 					key={index}
 					style={
 						index % 2
@@ -211,9 +224,9 @@ const Leagues: FunctionComponent<RouteComponentProps> = ({history: { push }}) =>
 							: { background: '#dddddd' }
 					}
 				>
-                    <td>{league.name}</td>
-                    <td>{league.country}</td>
-                    <td className="table-X">
+					<td>{league.name}</td>
+					<td>{league.country}</td>
+					<td className="table-X">
 						<IconButton
 							onClick={(event: React.MouseEvent<HTMLElement>) => {
 								//deleteLeague(league._id);
@@ -223,159 +236,186 @@ const Leagues: FunctionComponent<RouteComponentProps> = ({history: { push }}) =>
 							<FiXCircle />
 						</IconButton>
 					</td>
-                </tr>
-            )
-        })
-    }
+				</tr>
+			);
+		});
+	};
 
-    const generateTable = () => {
-        return(
-            <div>
+	const generateTable = () => {
+		return (
+			<div>
 				<table id="riders-list">
 					<tbody>
 						<tr>
 							<th>NAZWA</th>
-                            <th>KRAJ</th>
-                            <th>USUŃ</th>
+							<th>KRAJ</th>
+							<th>USUŃ</th>
 						</tr>
 						{renderTableData()}
 					</tbody>
 				</table>
 			</div>
-        )
-    }
+		);
+	};
 
-    const handleOnChangeName = () => (event: React.ChangeEvent<HTMLInputElement>) => {
-        event.persist();
-        if(event.target){
-            setAddLeagueName(event.target.value);
-        }
-    }
+	const handleOnChangeName = () => (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		event.persist();
+		if (event.target) {
+			setAddLeagueName(event.target.value);
+		}
+	};
 
-    const handleOnChangeCountry = () => event => {
-        event.persist();
-        if(event.target){
-            setAddLeagueCountry(event.target.value);
-        }
-    }
+	const handleOnChangeCountry = () => event => {
+		event.persist();
+		if (event.target) {
+			setAddLeagueCountry(event.target.value);
+		}
+	};
 
-    const handleOnSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        console.log("SUBMIT");
-        const validationResponse = validateLeagueData({name: addLeagueName, country: addLeagueCountry});
-        if (validationResponse.error){
-            setValidatedData(() => defaultValidatedData);
-            validationResponse.error.details.forEach(
-                (errorItem: ValidationErrorItem): any => {
-                    console.log(errorItem.message);
-                    setValidatedData((prevState: IValidatedData) => {
-                        return{
-                            ...prevState,
-                            [errorItem.path[0]]: {
-                                message: errorItem.message,
-                                error: true
-                            }
-                        };
-                    });
-                }
-            );
-        } else {
-            addLeague();
-        }
-    }
+	const handleOnSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+		const validationResponse = validateLeagueData({
+			name: addLeagueName,
+			country: addLeagueCountry
+		});
+		if (validationResponse.error) {
+			setValidatedData(() => defaultValidatedData);
+			validationResponse.error.details.forEach(
+				(errorItem: ValidationErrorItem): any => {
+					setValidatedData((prevState: IValidatedData) => {
+						return {
+							...prevState,
+							[errorItem.path[0]]: {
+								message: errorItem.message,
+								error: true
+							}
+						};
+					});
+				}
+			);
+		} else {
+			addLeague();
+		}
+	};
 
-    const generateDialog = () => {
-        return(
-            <>
-                <div>
-                    <Dialog open={openDialog} onClose={handleCloseDialog} className="leagues-dialog">
-                        <DialogTitle>
-                            <div className="leagues-dialog__header">
-                                <Typography variant="h4" className="leagues-dialog__title">
-                                    Dodawanie ligi
-                                </Typography>
-                                <IconButton
-                                    onClick={handleCloseDialog}
-                                    className="leagues-dialog__fix"
-                                >
-                                    <FiX />
-                                </IconButton>
-                            </div>
-                        </DialogTitle>
-                        <DialogContent dividers>
-                            <form className="leagues-dialog__form" onSubmit={handleOnSubmit}>
-                                <Grid container>
-                                    <Grid item xs={7} className="leagues-dialog__fields">
-                                        <FormControl className="leagues-dialog__text-field">
-                                            <TextField
-                                                label="Nazwa ligi"
-                                                required
-                                                autoComplete="league"
-                                                value={addLeagueName}
-                                                error={validatedData.name.error}
-                                                helperText={validatedData.name.message}
-                                                onChange={handleOnChangeName()}
-                                            />
-                                        </FormControl>
-                                        <FormControl className="leagues-dialog__select-field">
-                                            Kraj:
-                                            <Select
-                                                value={addLeagueCountry || ''}
-                                                onChange={handleOnChangeCountry()}
-                                            >
-                                                <MenuItem value="Polska">Polska</MenuItem>
-                                                <MenuItem value="Wielka Brytania">Wielka Brytania</MenuItem>
-                                                <MenuItem value="Szwecja">Szwecja</MenuItem>
-                                                <MenuItem value="Dania">Dania</MenuItem>
-                                            </Select>
-                                        </FormControl>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <Button
-                                            className="btn leagues-dialog__form_button"
-                                            type="submit"
-                                        >
-                                            Dodaj
-                                        </Button>
-                                    </Grid>
-                                </Grid>
-                            </form>
-                        </DialogContent>
-                    </Dialog>
-                </div>
-            </>
-        )
-    }
+	const generateDialog = () => {
+		return (
+			<>
+				<div>
+					<Dialog
+						open={openDialog}
+						onClose={handleCloseDialog}
+						className="leagues-dialog"
+					>
+						<DialogTitle>
+							<div className="leagues-dialog__header">
+								<Typography
+									variant="h4"
+									className="leagues-dialog__title"
+								>
+									Dodawanie ligi
+								</Typography>
+								<IconButton
+									onClick={handleCloseDialog}
+									className="leagues-dialog__fix"
+								>
+									<FiX />
+								</IconButton>
+							</div>
+						</DialogTitle>
+						<DialogContent dividers>
+							<form
+								className="leagues-dialog__form"
+								onSubmit={handleOnSubmit}
+							>
+								<Grid container>
+									<Grid
+										item
+										xs={7}
+										className="leagues-dialog__fields"
+									>
+										<FormControl className="leagues-dialog__text-field">
+											<TextField
+												label="Nazwa ligi"
+												required
+												autoComplete="league"
+												value={addLeagueName}
+												error={validatedData.name.error}
+												helperText={
+													validatedData.name.message
+												}
+												onChange={handleOnChangeName()}
+											/>
+										</FormControl>
+										<FormControl className="leagues-dialog__select-field">
+											Kraj:
+											<Select
+												value={addLeagueCountry || ''}
+												onChange={handleOnChangeCountry()}
+											>
+												<MenuItem value="Polska">
+													Polska
+												</MenuItem>
+												<MenuItem value="Wielka Brytania">
+													Wielka Brytania
+												</MenuItem>
+												<MenuItem value="Szwecja">
+													Szwecja
+												</MenuItem>
+												<MenuItem value="Dania">
+													Dania
+												</MenuItem>
+											</Select>
+										</FormControl>
+									</Grid>
+									<Grid item xs={12}>
+										<Button
+											className="btn leagues-dialog__form_button"
+											type="submit"
+										>
+											Dodaj
+										</Button>
+									</Grid>
+								</Grid>
+							</form>
+						</DialogContent>
+					</Dialog>
+				</div>
+			</>
+		);
+	};
 
-    useEffect(() => {
-        if (!userData.username) fetchUserData();
-    }, [])
+	useEffect(() => {
+		if (!userData.username) fetchUserData();
+	}, []);
 
-    return(
-        <>
-            <div className="leagues">
-                <div className="leagues__background"></div>
-                <Paper className="leagues__box">
-                    <Typography
-                        variant="h2"
-                        className="heading-1 leagues__heading"
-                    >
-                        Ligi
-                    </Typography>
-                    <IconButton
-                        onClick={handleOpenDialog}
-                        className="leagues__fiplus"
-                    >
-                        <FiPlus />
-                    </IconButton>
-                    <Divider />
-                    <br/>
-                    {generateTable()}
-                    {generateDialog()}
-                </Paper>
-            </div>
-        </>
-    )
-}
+	return (
+		<>
+			<div className="leagues">
+				<div className="leagues__background"></div>
+				<Paper className="leagues__box">
+					<Typography
+						variant="h2"
+						className="heading-1 leagues__heading"
+					>
+						Ligi
+					</Typography>
+					<IconButton
+						onClick={handleOpenDialog}
+						className="leagues__fiplus"
+					>
+						<FiPlus />
+					</IconButton>
+					<Divider />
+					<br />
+					{generateTable()}
+					{generateDialog()}
+				</Paper>
+			</div>
+		</>
+	);
+};
 
 export default Leagues;
