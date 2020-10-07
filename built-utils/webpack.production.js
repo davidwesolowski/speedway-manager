50279638ddb95f73de3052cd39069afe6447e8d4
Dawid Wesolowski <dawid.wesolowski96@gmail.com>
2020-10-07 17:28:11 +0200

    Added config for production

--- built-utils/webpack.production.js
+++ built-utils/webpack.production.js
@@ -1,7 +1,39 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = () => ({
-    output: {
-        filename: '[chunkhash].js' 
-    },
-    mode: 'production',
-    devtool: 'source-map',
+	mode: 'production',
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}
+		]
+	},
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true
+			})
+		]
+	},
+	plugins: [new MiniCssExtractPlugin({ filename: '[name].css' })]
 });
